036c48e499f850cf95024603d2b3bc4b1fbee90c
Thejas Eldhos Kurian <thejaseldhoskurian@gmail.com>
2023-02-20 13:29:54 +0530

    firstpythonprogram.py got modified

--- firstpythonprogram.py
+++ firstpythonprogram.py
@@ -1,3 +1,16 @@
+## Creating list
+# Method - 1
 x = []
 for i in range(10):
     x.append(i)
+
+# Method -2 
+x = [i for i in range(10)]
+
+# Basic Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+a = np.array([1,2,3])
+b = np.array([1],[2],[3])
